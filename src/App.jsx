53d47113718069c02aaf275cfbf2b9be9c777898
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-15 19:36:49 +0600

    added app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import Default from "./components/home/Default";
+import HomePage from "./pages/HomePage";
+import '../dist/style.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./components/forms/SignUp";
 
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Default/>}>
+            
+          </Route>
+          <Route path="home" element={<HomePage />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
