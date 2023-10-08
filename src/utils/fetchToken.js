bf702f470d02607462acd667b4a1722a3d0d2f20
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-08 16:29:50 +0600

    Add files via upload

--- src/utils/fetchToken.js
+++ src/utils/fetchToken.js
@@ -1,10 +1,16 @@
 
 const fetchAccessToken = async (url) => {
+
+    const refreshToken = JSON.parse(localStorage.getItem('refreshToken'));
+
     const data = await fetch(url, {
-       /*  headers : {
-            authorization : `Bearer ${refreshToken}`
-        }, */
-        headers : { "Content-Type" : "application/json"},
+        /*  headers : {
+             authorization : `Bearer ${refreshToken}`
+         }, */
+        headers: { 
+            'authorization' : `Bearer ${refreshToken}`,
+            "Content-Type": "application/json"
+        },
         credentials: 'include'
     });
     const response = await data.json();
